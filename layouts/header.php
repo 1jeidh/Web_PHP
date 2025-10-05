95d1275de91cf75263d160a585f0d6cb53a187b7
1jeidh <hungqh7@gmail.com>
2025-10-05 13:18:12 +0700

    Small modification to contact

--- layouts/header.php
+++ layouts/header.php
@@ -31,7 +31,7 @@
                             <a class="nav-link" href="#">Blog</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="contact.html">Contact Us</a>
+                            <a class="nav-link" href="contact.php">Contact Us</a>
                         </li>
                         <li class="nav-item">
                             <a href="cart.php"><i class="fa-solid fa-bag-shopping"></i></a>
